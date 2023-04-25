99af62bfae901e9a23c99afcb2a0d3c1a1008aae
aryansh316 <131454050+aryansh316@users.noreply.github.com>
2023-04-25 15:53:30 +0530

    Add files via upload

---
+++ crud-app/src/components/Employees.js
@@ -0,0 +1,24 @@
+const Employees =[
+    {
+        id :"1",
+        Name:"Joel Babox",
+        Age:"22"
+    },
+    {
+        id :"2",
+        Name:"Marry James",
+        Age:"23"
+    },
+    {
+        id :"3",
+        Name:"Robert Koffman",
+        Age:"24",
+    }    ,
+    {
+        id:'4',
+        Name:'Amy Watson',
+        Age:'25'
+    }
+]
+
+export default Employees;
